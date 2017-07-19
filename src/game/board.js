a8f637441c8f09ec86f0fab9994b9a25d1baf89a
Josmar Dias <josmarnet@gmail.com>
2017-07-19 02:42:04 -0300

    (game) detect winner

--- src/game/board.js
+++ src/game/board.js
@@ -1,6 +1,7 @@
 import {
   ID_EMPTY,
   ID_PLAYER_1,
+  ID_PLAYER_2,
 } from './constants'
 
 export const create = () => {
@@ -18,4 +19,30 @@ export const play = (state, playerId, gridPosition) => {
   const changedGrid = Object.assign([], state.grid, {[gridPosition]: state.playerTurn})
 
   return { ...state, grid: changedGrid }
+}
+
+const winPossibilities = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+export const getWinner = (state) => {
+  const grid = state.grid
+
+  /* adapted from
+   * https://github.com/israelhenrique/my-app/blob/40bd2d8fd5d6999a1eeac11ca3e6f2c0157876dc/src/index.js
+   */
+  for (let i = 0; i < winPossibilities.length; i++) {
+    const [a, b, c] = winPossibilities[i]
+    if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+      return grid[a]
+    }
+  }
+  return ID_EMPTY
 }
--- src/game/board.test.js
+++ src/game/board.test.js
@@ -1,6 +1,7 @@
 import {
   create,
   play,
+  getWinner,
 } from './board'
 
 import {
@@ -43,4 +44,56 @@ test('player makes the proper changes to the grid on his turn', () => {
   ]
 
   expect(state.grid).toEqual(expectedGrid)
+})
+
+test('no player is a winner', () => {
+  const actualGrid = [
+    ID_PLAYER_1, ID_PLAYER_1, ID_PLAYER_2,
+    ID_PLAYER_1, ID_PLAYER_1, ID_PLAYER_2,
+    ID_PLAYER_2, ID_PLAYER_2, ID_EMPTY,
+  ]
+  const actualState = {...initialState, grid: actualGrid}
+
+  const winner = getWinner(actualState)
+
+  expect(winner).toEqual(ID_EMPTY)
+})
+
+test('a player wins at horizontal', () => {
+  const actualGrid = [
+    ID_PLAYER_1, ID_PLAYER_1, ID_PLAYER_1,
+    ID_PLAYER_2, ID_PLAYER_2, ID_EMPTY,
+    ID_PLAYER_2, ID_PLAYER_2, ID_EMPTY,
+  ]
+  const actualState = {...initialState, grid: actualGrid}
+
+  const winner = getWinner(actualState)
+
+  expect(winner).toEqual(ID_PLAYER_1)
+})
+
+test('a player wins at vertical', () => {
+  const actualGrid = [
+    ID_PLAYER_1, ID_PLAYER_2, ID_PLAYER_2,
+    ID_PLAYER_1, ID_PLAYER_2, ID_PLAYER_2,
+    ID_PLAYER_1, ID_EMPTY, ID_EMPTY,
+  ]
+  const actualState = {...initialState, grid: actualGrid}
+
+  const winner = getWinner(actualState)
+
+  expect(winner).toEqual(ID_PLAYER_1)
+})
+
+test('a player wins at diagonal', () => {
+  const actualGrid = [
+    ID_PLAYER_1, ID_PLAYER_2, ID_PLAYER_2,
+    ID_PLAYER_2, ID_PLAYER_1, ID_PLAYER_2,
+    ID_PLAYER_2, ID_PLAYER_2, ID_PLAYER_1,
+  ]
+  const actualState = {...initialState, grid: actualGrid}
+
+  const winner = getWinner(actualState)
+
+  expect(winner).toEqual(ID_PLAYER_1)
 })
