8ffa247843c8980f94448d05881a46d38809722c
Josmar Dias <josmarnet@gmail.com>
2017-07-25 01:59:15 -0300

    add "Fork me on GitHub" ribbon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
 import 'normalize.css'
+import 'github-fork-ribbon-css/gh-fork-ribbon.css'
+
 import './App.css'
 
 import Game from './ui/game'
 
+const ForkMeOnGithub = () => (
+  <a
+    className="github-fork-ribbon"
+    href="https://github.com/josmardias/tic-tac-toe"
+    title="Fork me on GitHub"
+  >
+    Fork me on GitHub
+  </a>
+)
+
 const App = () => (
   <div id="app">
+    <ForkMeOnGithub />
     <Game />
   </div>
 )
