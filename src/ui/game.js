0d8733337b76d41950a9f97204035a577a9e5d70
Josmar Dias <josmarnet@gmail.com>
2017-07-25 01:22:18 -0300

    (ui) display match winner

--- src/ui/game.js
+++ src/ui/game.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import {
   create as createMatch,
   play as playOnMatch,
+  getWinner as getMatchWinner,
 } from '../game/match'
 
 import Board from './board'
@@ -13,6 +14,7 @@ export default class Game extends Component {
 
     this.state = {
       currentMatch: createMatch(),
+      playing: true,
     }
   }
 
@@ -20,26 +22,53 @@ export default class Game extends Component {
     return this.state.currentMatch
   }
 
+  stopGame() {
+    this.setState({ playing: false })
+  }
+
   updateCurrentMatch(match) {
     this.setState({ currentMatch: match })
   }
 
+  getCurrentWinner() {
+    return this.state.currentWinner
+  }
+
+  updateCurrentWinner(winner) {
+    if (!winner) {
+      return
+    }
+    this.setState({ currentWinner: winner })
+    this.stopGame()
+  }
+
   playOnTile(tileIndex) {
     const newMatchState = playOnMatch(this.getCurrentMatch(), tileIndex)
+    const winner = getMatchWinner(newMatchState)
+
     this.updateCurrentMatch(newMatchState)
+    this.updateCurrentWinner(winner)
   }
 
   handleBoardClick = (tileIndex) => {
+    if (!this.state.playing) {
+      return
+    }
     this.playOnTile(tileIndex)
   }
 
   render() {
+    const currentWinner = this.getCurrentWinner()
+
     return(
       <div>
         <Board
           grid={this.getCurrentMatch().grid}
           onClick={this.handleBoardClick}
         />
+        { currentWinner && (
+          `Winner: ${currentWinner}`
+        )}
       </div>
     )
   }
